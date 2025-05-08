5a75099bb54f52f75cc3cd22694f89b25d970ea6
Pontax02 <ppontanilla07@gmail.com>
2025-05-08 13:24:44 +0200

    init project

---
+++ src/main/java/edu/estatuas/galley/Galley.java
@@ -0,0 +1,10 @@
+package edu.estatuas.galley;
+
+/**
+ * Hello world!
+ */
+public class Galley {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

dfe80e3cbffbc85b64f5c59598a3c96a60bfbe6c
Pontax02 <ppontanilla07@gmail.com>
2025-05-08 14:22:09 +0200

    :rocket: build(Galley): Añadir codigo main

--- src/main/java/edu/estatuas/galley/Galley.java
+++ src/main/java/edu/estatuas/galley/Galley.java
@@ -1,10 +1,49 @@
 package edu.estatuas.galley;
 
 /**
- * Hello world!
+ *
+ * Bob Esponja trabaja como cocinero en el Krusty Krab,
+ * restaurante famoso por su hamburguesa de autor
+ * Krabby Patty.
+ * El restaurante tiene un menu llamado Galley Grub
+ * que puedes leer aquí:
+ * https://spongebob.fandom.com/wiki/Galley_Grub
+ *
+ * Las comandas son anotadas a mano por Squidward Tentacles.
+ * El propierario Eugene H. Krabs -un agarrao-
+ * en vez de adquirir un TPV ha decidido contratar
+ * a un estudiante de Dual para que programar un software
+ * para calcular el importe total del pedido e imprimir el recibo.
+ *
+ * Intenta hacer como que programas si no quieres terminar
+ * en la cocina ayudando a Bob a preparar Krusty Burguers.
  */
-public class Galley {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+public class Galley
+{
+    public static void main( String[] args )
+    {
+
+        /**
+         * Crea un diccionario con los precios
+         * de los extras.
+         *
+         *  Los precios de los extras son estos:
+         *  - cheese +0.25
+         *  - sauce +0.50
+         *  - medium +0.25
+         *  - large +0.50
+         *
+         * Lee los precios de los items en la imagen
+         * del menu proporcionada.
+         */
+
+        Prices.init_prices();
+
+        // formatea la salida de cada entrada del diccionario como prefieras
+        System.out.print("\n\t --- PRICES ---  \n");
+        Prices.display();
+
     }
+
+
 }

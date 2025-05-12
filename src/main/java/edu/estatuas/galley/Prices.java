e56806771f03d16018554503876995ba8827ce4b
Pontax02 <ppontanilla07@gmail.com>
2025-05-12 14:09:41 +0200

    :recycle: refactor(Prices): Sustituir la colección de los precios de los items por los extras

--- src/main/java/edu/estatuas/galley/Prices.java
+++ src/main/java/edu/estatuas/galley/Prices.java
@@ -11,15 +11,14 @@ public class Prices {
     Prices(){}
 
     static void init_prices() {
-        prices.put("KRABBY PATTY",1.25);
-        prices.put("CORAL BITS",1.00);
-        prices.put("GOLDEN LOAF",2.00);
-        prices.put("KELP RINGS",1.50);
-        prices.put("SEAFOM SODA",1.00);
+        prices.put("large",0.5);
+        prices.put("sauce",0.5);
+        prices.put("medium",0.25);
+        prices.put("cheese",0.25);
     }
     static void display(){
         for (Map.Entry<String, Double> entry : prices.entrySet()) {
-            System.out.println(entry.getKey() + ": " + entry.getValue());
+            System.out.println(entry.getKey() + "=" + entry.getValue());
         }
     }
 }

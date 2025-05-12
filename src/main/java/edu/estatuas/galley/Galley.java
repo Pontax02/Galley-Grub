3bff605e66e0b6d731a96f645979abf17326cf0e
Pontax02 <ppontanilla07@gmail.com>
2025-05-12 15:15:12 +0200

    :rocket: build(Galley): Añadir codigo main e importaciones

--- src/main/java/edu/estatuas/galley/Galley.java
+++ src/main/java/edu/estatuas/galley/Galley.java
@@ -2,7 +2,8 @@ package edu.estatuas.galley;
 
 import edu.estatuas.galley.products.Product;
 import edu.estatuas.galley.products.Item;
-
+import edu.estatuas.galley.comanda.Order;
+import edu.estatuas.galley.comanda.Comanda;
 /**
  *
  * Bob Esponja trabaja como cocinero en el Krusty Krab,
@@ -72,6 +73,53 @@ public class Galley
         display(loaf);
         Product soda = new Item("Seafoam Soda", 1.00);
         display(soda);
+
+        /**
+         * Bubble Bass llega al restaurante
+         * y hace una comanda muy simple.
+         * Quiere todo lo que hay en el menu
+         * en su tamaño estandar y sin
+         * condimentos extras.
+         *
+         * Squidward Tentacles crea la comanda
+         * y la muestra en la pantalla para que
+         * el cliente le de el visto bueno
+         * antes de generar el recibo con la factura.
+         *
+         *
+         * OPCIONAL:
+         * Puesto que a lo largo del dia van a generarse muchos objetos
+         * siempre del mismo tipo, vamos a ahorrar memoria.
+         *
+         * Implementa la clase ItemFactory siguiendo los casos test
+         * que se proporcionan.
+         *
+         * ItemFactory crea el Item que le pides,
+         * lo guarda en un diccionario y lo devuelve.
+         * Cuando solicitas crear de nuevo ese Item
+         * te devuelve la misma referencia al objeto que has creado antes,
+         * ahorrando asi memoria.
+         */
+
+        Comanda order = new Order();
+
+        order.addItem("Krabby Patty", 1.25);
+        order.addItem("Coral Bits", 1.00);
+        order.addItem("Kelp Rings", 1.50);
+        order.addItem("Golden Loaf", 2.00);
+        order.addItem("Seafoam Soda", 1.00);
+
+        order.display();
+
+        /**
+         * Squidward Tentacles genera el recibo,
+         * la app calcula el importe total de la comanda
+         * y lo muestra en pantalla.
+         */
+
+        Ticket receipt = new Receipt(order);
+        receipt.total();
+        receipt.print();
 
     }
     public static void display(Product item) {

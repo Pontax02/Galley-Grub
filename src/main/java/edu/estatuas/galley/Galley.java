ffb66c50e5bb4516a03567da7799912b9554544f
Pontax02 <ppontanilla07@gmail.com>
2025-05-12 14:38:16 +0200

    :tada: build(Galley): Añadir importaciones y completar la primera HU

--- src/main/java/edu/estatuas/galley/Galley.java
+++ src/main/java/edu/estatuas/galley/Galley.java
@@ -1,5 +1,8 @@
 package edu.estatuas.galley;
 
+import edu.estatuas.galley.products.Product;
+import edu.estatuas.galley.products.Item;
+
 /**
  *
  * Bob Esponja trabaja como cocinero en el Krusty Krab,
@@ -42,8 +45,37 @@ public class Galley
         // formatea la salida de cada entrada del diccionario como prefieras
         System.out.print("\n\t --- PRICES ---  \n");
         Prices.display();
+        /**
+         * Crea los items del menu
+         * y printa sus datos.
+         *
+         * Krabby Patty
+         * Coral Bits
+         * Kelp Rings
+         * Golden Loaf
+         * Seafoam Soda
+         *
+         * Incluye los metodos sobrescritos
+         * equals y hascode para comparar objetos
+         * de tipo Item.
+         */
+
+        System.out.print("\n\t --- GALLEY GRUB ---  \n");
+
+        Product patty = new Item("Krabby Patty", 1.25);
+        display(patty);
+        Product bits = new Item("Coral Bits", 1.00);
+        display(bits);
+        Product rings = new Item("Kelp Rings", 1.50);
+        display(rings);
+        Product loaf = new Item("Golden Loaf", 2.00);
+        display(loaf);
+        Product soda = new Item("Seafoam Soda", 1.00);
+        display(soda);
 
     }
-
+    public static void display(Product item) {
+        System.out.print("\t" + item.toString() + "\n");
+    }
 
 }

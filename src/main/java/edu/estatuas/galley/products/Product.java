2d76052a321471885e3543efaef7668e3807bf4e
Pontax02 <ppontanilla07@gmail.com>
2025-05-12 14:35:42 +0200

    :sparkles: feat(Product): Añadir interfaz Product

---
+++ src/main/java/edu/estatuas/galley/products/Product.java
@@ -0,0 +1,8 @@
+package edu.estatuas.galley.products;
+
+public interface Product {
+        String getName();
+        double getPrice();
+        String getExtra();
+        boolean isRegular();
+}
